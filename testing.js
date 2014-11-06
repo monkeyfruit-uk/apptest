1b008a6396f642986762ea502fb6ae5ff0456e2b
Monkeyfruit <developer@monkeyfruit.co.uk>
2014-11-06 02:49:02 +0000

    Updated test script

--- testing.js
+++ testing.js
@@ -1,6 +1,7 @@
 window.testing = new (function(){
 
 	var me = this;
+	var shortTimeout = 2000;
 	var defaultTimeout = 6000; // 5 seconds
 	var extendedTimeout = 10000; // 10 seconds
 
@@ -18,10 +19,26 @@ window.testing = new (function(){
 		}, defaultTimeout);
 	};
 
+	me.moreScreen = function(ref){
+		setTimeout(function(){
+			$('a.more').click();
+			setTimeout(function(){
+				$('.moreList a').first().click();
+				setTimeout(function(){
+					$('.cameraContent .camera').click();
+					setTimeout(function(){
+						$('#photo-take').click();
+					}, extendedTimeout);
+				}, extendedTimeout);
+			}, extendedTimeout);
+		}, shortTimeout);
+	}
+
 
 	// 
 	me.start = function(){
-		me.adviceScreen();
+		//me.adviceScreen();
+		me.moreScreen();
 	}
 
 	//
